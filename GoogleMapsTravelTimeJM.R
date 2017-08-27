47a5324d24603bc172668ffd493e8fbb613e0961
AJ Duncanson <andrew@duncanson.net.au>
2017-08-27 16:09:46 +1000

    Update GoogleMapsTravelTimeJM.R

--- GoogleMapsTravelTimeJM.R
+++ GoogleMapsTravelTimeJM.R
@@ -1,8 +1,23 @@
 install.packages("gmapsdistance")
 library(gmapsdistance)
-results = gmapsdistance(origin = c("UTS+Building+11+Ultimo+NSW","UTS+Building+11+Ultimo+NSW","UTS+Building+11+Ultimo+NSW"), 
-                        destination = c("Central+Station+Sydney+NSW","Hornsby+Station+Hornsby+NSW","Parramatta+Station+Parramatta+NSW"), 
-                        mode = "driving", 
-                        dep_date = "2017-08-31", 
-                        dep_time = "20:40:00")
+
+#just expanding on @moonjeremy's code, with extra arguments
+
+results = gmapsdistance(
+  #use a vector of origins and a vector of destinations and ask for all combinations
+  #can use lat/long, which may facilitate creating lots of points
+          origin = c("UTS+Building+11+Ultimo+NSW","-33.883013,151.149366"), 
+          destination = c("Central+Station+Sydney+NSW","Hornsby+Station+Hornsby+NSW","Parramatta+Station+Parramatta+NSW"), 
+          combinations = "all",
+          mode = "driving", 
+  #best guess... or can ask for optimistic or pessimistic
+          traffic_model = "best_guess",
+          dep_date = "2017-08-27",
+          dep_time = "08:00:00",
+          shape = "long",
+  #all the interesting arguments need an API key from https://developers.google.com/maps/documentation/distance-matrix/
+  #and you can run 2500 individual origin-destination pairs per day for free, then it's 50 US cents per 1000
+  #best go get your own and have a play
+          key = "")
+
 results
